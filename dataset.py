222fc2fece6eaaca34c49573252c80bbdf4503c3
Sergio Moises <104019653+sermomac@users.noreply.github.com>
2023-05-05 16:02:03 +0530

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,102 @@
+import os
+import cv2
+import gc
+import numpy as np
+import pandas as pd
+import itertools
+from tqdm.autonotebook import tqdm
+import albumentations as A
+import matplotlib.pyplot as plt
+import torch
+
+
+df = pd.read_csv("../../file.csv", delimiter="|")
+df.columns = ['SN', 'image', 'caption']
+df['caption'] = df['caption'].str.lstrip()
+df['caption_number'] = df['caption_number'].str.lstrip()
+df.loc[19999, 'SN'] = "10"
+df.loc[19999, 'caption'] = "The cat runs after the mouse."  # Default
+ids = [id_ for id_ in range(len(df) // 5) for i in range(5)]
+df['id'] = ids
+df.to_csv("captions.csv", index=False)  # For multiple caption
+df.head()
+
+
+class CFG:
+    debug = False
+    image_path = "../../images"
+    captions_path = "."
+    batch_size = 32
+    num_workers = 4
+    head_lr = 1e-3
+    image_encoder_lr = 1e-4
+    text_encoder_lr = 1e-5
+    weight_decay = 1e-3
+    patience = 1
+    factor = 0.8
+    epochs = 2
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_name = 'resnet50'
+    image_embedding = 2048
+    text_encoder_model = "distilbert-base-uncased"
+    text_embedding = 768
+    text_tokenizer = "distilbert-base-uncased"
+    max_length = 200
+
+    pretrained = True  # for both image encoder and text encoder
+    trainable = True  # for both image encoder and text encoder
+    temperature = 1.0
+
+    # image size
+    size = 224
+
+    # for projection head; used for both image and text encoders
+    num_projection_layers = 1
+    projection_dim = 256
+    dropout = 0.1
+
+
+class CLIPDataset(torch.utils.data.Dataset):
+    def __init__(self, image_filenames, captions, tokenizer, transforms):
+
+        self.image_filenames = image_filenames  # used through the file names
+        self.captions = list(captions)  # For an image with multiple captions
+        self.encoded_captions = tokenizer(
+            list(captions), padding=True, truncation=True, max_length=CFG.max_length
+        )
+        self.transforms = transforms
+
+    def __getitem__(self, idx):
+        item = {
+            key: torch.tensor(values[idx])
+            for key, values in self.encoded_captions.items()
+        }
+
+        image = cv2.imread(f"{CFG.image_path}/{self.image_filenames[idx]}")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = self.transforms(image=image)['image']
+        item['image'] = torch.tensor(image).permute(2, 0, 1).float()
+        item['caption'] = self.captions[idx]
+
+        return item
+
+    def __len__(self):
+        return len(self.captions)
+
+
+def get_transforms(mode="train"):
+    if mode == "train":
+        return A.Compose(
+            [
+                A.Resize(CFG.size, CFG.size, always_apply=True),
+                A.Normalize(max_pixel_value=255.0, always_apply=True),
+            ]
+        )
+    else:
+        return A.Compose(
+            [
+                A.Resize(CFG.size, CFG.size, always_apply=True),
+                A.Normalize(max_pixel_value=255.0, always_apply=True),
+            ]
+        )
