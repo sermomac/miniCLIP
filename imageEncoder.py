4d32b30e718a9e166bb6ee3537a13b5f311becc0
Sergio Moises <104019653+sermomac@users.noreply.github.com>
2023-05-10 11:52:33 +0530

    Add files via upload

---
+++ imageEncoder.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import timm
+from transformers import DistilBertModel, DistilBertConfig, DistilBertTokenizer
+
+
+#The following code is redudant, NILESH please see to it
+class CFG:
+    debug = False
+    image_path = "../../images"
+    captions_path = "."
+    batch_size = 32
+    num_workers = 4
+    head_lr = 1e-3
+    image_encoder_lr = 1e-4
+    text_encoder_lr = 1e-5
+    weight_decay = 1e-3
+    patience = 1
+    factor = 0.8
+    epochs = 2
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model_name = 'resnet50'
+    image_embedding = 2048
+    text_encoder_model = "distilbert-base-uncased"
+    text_embedding = 768
+    text_tokenizer = "distilbert-base-uncased"
+    max_length = 200
+
+    pretrained = True  # for both image encoder and text encoder
+    trainable = True  # for both image encoder and text encoder
+    temperature = 1.0
+
+    # image size
+    size = 224
+
+    # for projection head; used for both image and text encoders
+    num_projection_layers = 1
+    projection_dim = 256
+    dropout = 0.1
+
+
+class ImageEncoder(nn.Module):
+
+    def __init__(
+        self, model_name=CFG.model_name, pretrained=CFG.pretrained, trainable=CFG.trainable
+    ):
+        super().__init__()
+        self.model = timm.create_model(
+            model_name, pretrained, num_classes=0, global_pool="avg"
+        )
+        for p in self.model.parameters():
+            p.requires_grad = trainable
+
+    def forward(self, x):
+        return self.model(x)
+
+#///////////////////////////////// Alternative functions for encoding 
+    def __init__(self):
+        super(ImageEncoder, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+        return x
